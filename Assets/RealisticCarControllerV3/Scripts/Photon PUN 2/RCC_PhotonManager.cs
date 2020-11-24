3f298b420121ac37e3fedf615b431bc8fecdb4be
PiotrDevPro <proplaya7880281@gmail.com>
2020-11-24 22:02:15 +0300

    Network mode for players

---
+++ Assets/RealisticCarControllerV3/Scripts/Photon PUN 2/RCC_PhotonManager.cs
@@ -0,0 +1,114 @@
+﻿//----------------------------------------------
+//            Realistic Car Controller
+//
+// Copyright © 2014 - 2019 BoneCracker Games
+// http://www.bonecrackergames.com
+// Buğra Özdoğanlar
+//
+//----------------------------------------------
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Connects to Photon Server, registers the player, and activates player UI panel when connected.
+/// </summary>
+[AddComponentMenu("BoneCracker Games/Realistic Car Controller/Network/Photon/RCC Photon Scene Manager")]
+public class RCC_PhotonManager : Photon.Pun.MonoBehaviourPunCallbacks {
+
+	//public InputField playerName;
+
+	void Start () {
+
+		ConnectToServer();
+	}
+
+	void ConnectToServer () {
+
+		print("Connecting to photon server");
+		RCC_InfoLabel.Instance.ShowInfo ("Connecting to photon server");
+
+		if (!Photon.Pun.PhotonNetwork.IsConnectedAndReady) {
+
+			Photon.Pun.PhotonNetwork.ConnectUsingSettings ();
+
+		}
+
+		if (Photon.Pun.PhotonNetwork.IsConnectedAndReady) {
+
+		}
+	
+	}
+
+	public override void OnConnectedToMaster(){
+
+		print ("Connected to master server");
+		Photon.Pun.PhotonNetwork.JoinLobby ();
+
+	}
+
+	void OnGUI(){
+
+		if(!Photon.Pun.PhotonNetwork.IsConnectedAndReady)
+			GUI.color = Color.red;
+		GUILayout.Label("State: " + Photon.Pun.PhotonNetwork.NetworkClientState.ToString());
+		GUI.color = Color.white;
+		GUILayout.Label("Name: " + PhotonNetwork.NickName);
+		GUILayout.Label("Total Players: " + Photon.Pun.PhotonNetwork.PlayerList.Length.ToString());
+		GUILayout.Label("Ping: " + Photon.Pun.PhotonNetwork.GetPing().ToString());
+
+	}
+
+	public override void OnJoinedLobby(){
+
+		print("Joined lobby");
+		RCC_InfoLabel.Instance.ShowInfo ("Joined Lobby");
+		Photon.Pun.PhotonNetwork.JoinRandomRoom();
+
+	}
+
+	public override void OnJoinRandomFailed(short a, string b){
+
+		print("Joining to random room has failed!, Creating new room...");
+		RCC_InfoLabel.Instance.ShowInfo ("Joining to random room has failed!, Creating new room...");
+		Photon.Pun.PhotonNetwork.CreateRoom(null);
+
+	}
+
+	
+
+	public override void OnJoinedRoom(){
+
+		print("Joined room");
+		RCC_InfoLabel.Instance.ShowInfo ("Joined Room. You can spawn your vehicle.");
+		//playerName.gameObject.SetActive(true);
+
+	}
+
+	public void Leave()
+	{
+		PhotonNetwork.LeaveRoom();
+		Time.timeScale = 1;
+	}
+
+	public override void OnLeftRoom()
+	{
+		SceneManager.LoadScene("garage");
+		PhotonNetwork.Disconnect();
+		Amplitude.Instance.logEvent("PlayerLeaveTheRoom");
+
+	}
+
+
+	public void SetPlayerName(string name){
+
+		Photon.Pun.PhotonNetwork.NickName = name;
+		//playerName.gameObject.SetActive(false);
+		//RCC_SceneManager.Instance.activePlayerCanvas.SetDisplayType (RCC_UIDashboardDisplay.DisplayType.Full);
+
+	}
+
+}
